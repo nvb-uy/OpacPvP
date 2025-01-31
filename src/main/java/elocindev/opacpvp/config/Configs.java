c9a6272280e45ec309e2b1a1dfb1088b335732cd
nvb-uy <elocindev@lunapixel.gg>
2025-01-31 15:55:35 -0300

    fix(config issues)

--- src/main/java/elocindev/opacpvp/config/Configs.java
+++ src/main/java/elocindev/opacpvp/config/Configs.java
@@ -4,11 +4,12 @@ import elocindev.opacpvp.config.entries.OpacPvPConfig;
 import elocindev.necronomicon.api.config.v1.NecConfigAPI;
 
 public class Configs {
-    public static OpacPvPConfig MAIN = OpacPvPConfig.INSTANCE;
+    public static OpacPvPConfig MAIN;
 
     public static void loadCommonConfigs() {
         //?if fabric {
         NecConfigAPI.registerConfig(OpacPvPConfig.class);
+        MAIN = OpacPvPConfig.INSTANCE;
         //?}
     }
 
